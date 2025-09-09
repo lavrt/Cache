3208ed9ecd1c311032060aa1006071757d867455
Alexey <lavrik-aleksey-1@yandex.ru>
2025-09-10 00:45:57 +0300

    made the RemoveLast() private

--- include/cache.hpp
+++ include/cache.hpp
@@ -22,17 +22,6 @@ private:
     size_t a1_out_capacity = 0;
     std::unordered_set<keyT> a1_out;
 
-public:
-    TwoQCache(size_t sz) {
-        if (sz == 0) {
-            throw std::invalid_argument("The cache size cannot be zero");
-        }
-        
-        am_capacity = static_cast<size_t>(sz * 0.75);
-        a1_in_capacity = sz - am_capacity;
-        a1_out_capacity = a1_in_capacity;
-    }
-
     keyT RemoveLast(std::list<std::pair<keyT, T>>& list, std::unordered_map<keyT, ListIter>& hash) {
         if (list.empty()) {
             throw std::runtime_error("Attempt to delete an element from an empty list");
@@ -43,6 +32,17 @@ public:
         hash.erase(last_key);
 
         return last_key;
+    }
+
+public:
+    TwoQCache(size_t sz) {
+        if (sz == 0) {
+            throw std::invalid_argument("The cache size cannot be zero");
+        }
+
+        am_capacity = static_cast<size_t>(sz * 0.75);
+        a1_in_capacity = sz - am_capacity;
+        a1_out_capacity = a1_in_capacity;
     }
 
     void PrintCache() { // NOTE debug
@@ -71,18 +71,14 @@ public:
             return true;
         } else if (auto hit_a1_in = a1_in_hash.find(key); hit_a1_in != a1_in_hash.end()) {
             if (am.size() == am_capacity) {
-                keyT lru_elem_key = am.back().first;
-                am.pop_back();
-                am_hash.erase(lru_elem_key);
+                RemoveLast(am, am_hash);
             }
             am.splice(am.begin(), a1_in, hit_a1_in->second);
             a1_in_hash.erase(hit_a1_in->first);
             return true;
         } else if (a1_out.contains(key)) {
             if (am.size() == am_capacity) {
-                keyT lru_elem_key = am.back().first;
-                am.pop_back();
-                am_hash.erase(lru_elem_key);
+                RemoveLast(am, am_hash);
             }
             
             am.emplace_front(key, SlowGetPage(key));
@@ -91,10 +87,7 @@ public:
             return false;
         } else {
             if (a1_in.size() == a1_in_capacity) {
-                keyT lru_elem_key = a1_in.back().first;
-                a1_in.pop_back();
-                a1_in_hash.erase(lru_elem_key);
-                a1_out.insert(lru_elem_key);
+                a1_out.insert(RemoveLast(a1_in, a1_in_hash));
             }
 
             a1_in.emplace_front(key, SlowGetPage(key));
