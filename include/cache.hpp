cfe908b09bd176f28ba2dcc289b0fac8aaa000b5
Alexey <lavrik-aleksey-1@yandex.ru>
2025-09-10 21:52:33 +0300

    fix naming, fix bug with iterator invalidation

--- include/cache.hpp
+++ include/cache.hpp
@@ -13,17 +13,17 @@ class TwoQCache {
 private:
     const double kAmRatio = 0.75;
 
-    size_t am_capacity = 0;
-    std::list<std::pair<keyT, T>> am;
-    std::unordered_map<keyT, ListIter> am_hash;
+    size_t am_capacity_ = 0;
+    std::list<std::pair<keyT, T>> am_;
+    std::unordered_map<keyT, ListIter> am_hash_;
 
-    size_t a1_in_capacity = 0;
-    std::list<std::pair<keyT, T>> a1_in;
-    std::unordered_map<keyT, ListIter> a1_in_hash;
+    size_t a1_in_capacity_ = 0;
+    std::list<std::pair<keyT, T>> a1_in_;
+    std::unordered_map<keyT, ListIter> a1_in_hash_;
 
-    size_t a1_out_capacity = 0;
-    std::list<keyT> a1_out;
-    std::unordered_set<keyT> a1_out_hash;
+    size_t a1_out_capacity_ = 0;
+    std::list<keyT> a1_out_;
+    std::unordered_set<keyT> a1_out_hash_;
 
     keyT RemoveLast(std::list<std::pair<keyT, T>>& list, std::unordered_map<keyT, ListIter>& hash) {
         if (list.empty()) {
@@ -47,66 +47,78 @@ private:
 
 public:
     TwoQCache(size_t sz) {
-        am_capacity = static_cast<size_t>(sz * kAmRatio);
-        a1_in_capacity = sz - am_capacity;
-        a1_out_capacity = a1_in_capacity;
-        if (am_capacity <= 0 || a1_in_capacity <= 0 || a1_out_capacity <= 0) {
+        am_capacity_ = static_cast<size_t>(sz * kAmRatio);
+        a1_in_capacity_ = sz - am_capacity_;
+        a1_out_capacity_ = a1_in_capacity_;
+        if (am_capacity_ <= 0 || a1_in_capacity_ <= 0 || a1_out_capacity_ <= 0) {
             throw std::invalid_argument("The cache size is too small");
         }
     }
 
-    void PrintCache() { // NOTE debug
-        std::cout << "________________________________\n";
-        std::cout << "Am(" << am_capacity << "): ";
-        for (const auto& elem : am) {
+    void PrintCache() const { // NOTE debug
+        std::cout << "++++++++++++++++++++++++++++++++\n";
+        std::cout << "Am(" << am_capacity_ << "): ";
+        for (const auto& elem : am_) {
             std::cout << elem.second << " ";
         }
-        std::cout << "\nA1_in(" << a1_in_capacity << "): ";
-        for (const auto& elem : a1_in) {
+        std::cout << "\nAm_hash:\n";
+        for (const auto& elem : am_hash_) {
+            std::cout << elem.first << " ";
+        }
+        std::cout << "\nA1_in(" << a1_in_capacity_ << "): ";
+        for (const auto& elem : a1_in_) {
             std::cout << elem.second << " ";
         } 
-        std::cout << "\nA1_out(" << a1_out_capacity << "): ";
-        for (const auto& elem : a1_out) {
+        std::cout << "\na1_in_hash_:\n";
+        for (const auto& elem : a1_in_hash_) {
+            std::cout << elem.first << " ";
+        }
+        std::cout << "\nA1_out(" << a1_out_capacity_ << "): ";
+        for (const auto& elem : a1_out_) {
             std::cout << elem << " ";
         }
-        std::cout << "\n________________________________\n";
+        std::cout << "\na1_out_hash_:\n";
+        for (const auto& elem : a1_out_hash_) {
+            std::cout << elem << " ";
+        }
+        std::cout << "\n++++++++++++++++++++++++++++++++\n";
     }
 
     template <typename F>
     bool LookupUpdate(keyT key, F SlowGetPage) {
-        if (auto hit = am_hash.find(key); hit != am_hash.end()) {
-            if (hit->second != am.begin()) {
-                am.splice(am.begin(), am, hit->second);
+        if (auto hit = am_hash_.find(key); hit != am_hash_.end()) {
+            if (hit->second != am_.begin()) {
+                am_.splice(am_.begin(), am_, hit->second);
             }
             return true;
-        } else if (auto hit = a1_in_hash.find(key); hit != a1_in_hash.end()) {
-            if (am.size() == am_capacity) {
-                RemoveLast(am, am_hash);
+        } else if (auto hit = a1_in_hash_.find(key); hit != a1_in_hash_.end()) {
+            if (am_.size() == am_capacity_) {
+                RemoveLast(am_, am_hash_);
             }
-            am.splice(am.begin(), a1_in, hit->second);
-            a1_in_hash.erase(hit->first);
-            am_hash[hit->first] = am.begin();
+            am_.splice(am_.begin(), a1_in_, hit->second);
+            am_hash_[hit->first] = am_.begin();
+            a1_in_hash_.erase(hit->first);
             return true;
-        } else if (a1_out_hash.contains(key)) {
-            if (am.size() == am_capacity) {
-                RemoveLast(am, am_hash);
+        } else if (a1_out_hash_.contains(key)) {
+            if (am_.size() == am_capacity_) {
+                RemoveLast(am_, am_hash_);
             }
-            am.emplace_front(key, SlowGetPage(key));
-            am_hash[key] = am.begin();
-            a1_out.remove(key);
-            a1_out_hash.erase(key);
+            am_.emplace_front(key, SlowGetPage(key));
+            am_hash_[key] = am_.begin();
+            a1_out_.remove(key);
+            a1_out_hash_.erase(key);
             return false;
         } else {
-            if (a1_in.size() == a1_in_capacity) {
-                if (a1_out.size() == a1_out_capacity) {
-                    RemoveLast(a1_out, a1_out_hash);
+            if (a1_in_.size() == a1_in_capacity_) {
+                if (a1_out_.size() == a1_out_capacity_) {
+                    RemoveLast(a1_out_, a1_out_hash_);
                 }
-                keyT last_key = RemoveLast(a1_in, a1_in_hash);
-                a1_out_hash.insert(last_key);
-                a1_out.emplace_front(last_key);
+                keyT last_key = RemoveLast(a1_in_, a1_in_hash_);
+                a1_out_hash_.insert(last_key);
+                a1_out_.emplace_front(last_key);
             }
-            a1_in.emplace_front(key, SlowGetPage(key));
-            a1_in_hash[key] = a1_in.begin();
+            a1_in_.emplace_front(key, SlowGetPage(key));
+            a1_in_hash_[key] = a1_in_.begin();
             return false;
         }
     }
