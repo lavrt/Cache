77ab295fbf51d4b870cf62c83a77cc131a0d801a
Alexey <lavrik-aleksey-1@yandex.ru>
2025-09-10 22:32:05 +0300

    minor fixes

--- include/cache.hpp
+++ include/cache.hpp
@@ -6,12 +6,14 @@
 #include <unordered_map>
 #include <unordered_set>
 
+namespace caches {
+
 template <typename T, typename keyT = int>
 class TwoQCache {
     using ListIter = typename std::list<std::pair<keyT, T>>::iterator;
     
 private:
-    const double kAmRatio = 0.75;
+    static constexpr double kDefaultAmRatio = 0.75;
 
     size_t am_capacity_ = 0;
     std::list<std::pair<keyT, T>> am_;
@@ -46,10 +48,15 @@ private:
     }
 
 public:
-    TwoQCache(size_t sz) {
-        am_capacity_ = static_cast<size_t>(sz * kAmRatio);
+    TwoQCache(size_t sz, double am_ratio = kDefaultAmRatio) {
+        if (am_ratio <= 0.0 || am_ratio >= 1.0) {
+            throw std::invalid_argument("AM ratio must be in the range (0,1)");
+        }
+
+        am_capacity_ = static_cast<size_t>(sz * am_ratio);
         a1_in_capacity_ = sz - am_capacity_;
         a1_out_capacity_ = a1_in_capacity_;
+
         if (am_capacity_ <= 0 || a1_in_capacity_ <= 0 || a1_out_capacity_ <= 0) {
             throw std::invalid_argument("The cache size is too small");
         }
@@ -99,14 +106,14 @@ public:
             am_hash_[hit->first] = am_.begin();
             a1_in_hash_.erase(hit->first);
             return true;
-        } else if (a1_out_hash_.contains(key)) {
+        } else if (auto hit = a1_out_hash_.find(key); hit != a1_out_hash_.end()) {
             if (am_.size() == am_capacity_) {
                 RemoveLast(am_, am_hash_);
             }
             am_.emplace_front(key, SlowGetPage(key));
             am_hash_[key] = am_.begin();
             a1_out_.remove(key);
-            a1_out_hash_.erase(key);
+            a1_out_hash_.erase(*hit);
             return false;
         } else {
             if (a1_in_.size() == a1_in_capacity_) {
@@ -123,5 +130,7 @@ public:
         }
     }
 };
+
+} // namespace caches
 
 #endif // CACHE_HPP
