79b86d100b17b9d44433ab537eeae379815d5167
Alexey <lavrik-aleksey-1@yandex.ru>
2025-09-12 17:26:16 +0300

    implemented comparative tests

--- tests/tests.cc
+++ tests/tests.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cassert>
+#include <iomanip>
 
 #include "cache.hpp"
 
@@ -9,6 +10,7 @@ namespace colors {
     const std::string kRed = "\033[31m";
     const std::string kGreen = "\033[32m";
     const std::string kReset = "\033[0m";
+    const std::string kBrightBlue = "\033[94m";
 };
 
 // static ------------------------------------------------------------------------------------------
@@ -22,35 +24,40 @@ static const std::vector<Test> kTwoQCacheTests {
 };
 
 static const std::vector<Test> kIdealCacheTests {
-    {"ideal_test_1", 3, {1, 2, 3, 1, 4, 2, 5, 3, 4, 5},    {0, 0, 0, 1, 0, 1, 0, 1, 1, 1}},
-    {"ideal_test_2", 2, {1, 2, 1, 3, 2, 4, 1, 5},          {0, 0, 1, 0, 1, 0, 0, 0}},
-    {"ideal_test_3", 4, {1, 2, 3, 4, 1, 2, 5, 1, 2, 3},    {0, 0, 0, 0, 1, 1, 0, 1, 1, 1}},
-    {"ideal_test_4", 1, {1, 1, 1, 1, 1},                   {0, 1, 1, 1, 1}},
-    {"ideal_test_5", 3, {1, 2, 3, 4, 5, 1, 2, 3},          {0, 0, 0, 0, 0, 1, 1, 0}},
+    {"ideal_test_1", 3, {1, 2, 3, 1, 4, 2, 5, 3, 4, 5},     {0, 0, 0, 1, 0, 1, 0, 1, 1, 1}},
+    {"ideal_test_2", 2, {1, 2, 1, 3, 2, 4, 1, 5},           {0, 0, 1, 0, 1, 0, 0, 0}},
+    {"ideal_test_3", 4, {1, 2, 3, 4, 1, 2, 5, 1, 2, 3},     {0, 0, 0, 0, 1, 1, 0, 1, 1, 1}},
+    {"ideal_test_4", 1, {1, 1, 1, 1, 1},                    {0, 1, 1, 1, 1}},
+    {"ideal_test_5", 3, {1, 2, 3, 4, 5, 1, 2, 3},           {0, 0, 0, 0, 0, 1, 1, 0}},
 };
 
-static void RunTwoQCahceTests();
+static const std::string kLine = "=======================================\n";
+
+static void RunTwoQCacheTests();
 static void RunIdealCacheTests();
+static void RunComparativeTests(size_t cache_capacity);
 
 // global ------------------------------------------------------------------------------------------
 
 void RunTests() {
     std::cout << "Running cache tests\n";
-    std::cout << "=======================================\n";
+    std::cout << kLine;
 
-    RunTwoQCahceTests();
+    RunTwoQCacheTests();
     RunIdealCacheTests();
+    RunComparativeTests(3);
     
-    std::cout << "=======================================\n";
     std::cout << "Testing complete\n";
 }
 
 // static ------------------------------------------------------------------------------------------
 
-static void RunTwoQCahceTests() {
+static void RunTwoQCacheTests() {
     std::cout << "Running 2Q cache tests\n";
     
     for (const Test& test : kTwoQCacheTests) {
+        std::cout << test.test_name_ << ": ";
+
         assert(test.requests_.size() == test.expected_hits_.size());
 
         caches::TwoQCache<int> cache{test.cache_size_};
@@ -60,22 +67,28 @@ static void RunTwoQCahceTests() {
             bool expected_hit = test.expected_hits_[i];
 
             if (actual_hit != expected_hit) {
-                throw std::runtime_error(colors::kRed + "Test " + test.test_name_ +
-                                         ": Request " + std::to_string(i + 1) + 
+                std::cout << colors::kRed << "failed" << colors::kReset << std::endl;
+
+                throw std::runtime_error(colors::kRed + "Request " + std::to_string(i + 1) + 
                                          " (key=" + std::to_string(test.requests_[i]) + 
                                          "): expected " + (expected_hit ? "HIT" : "MISS") + 
                                          ", got " + (actual_hit ? "HIT" : "MISS") + colors::kReset);
             }
         }
+
+        std::cout << colors::kGreen << "passed" << colors::kReset << std::endl;
     }
 
     std::cout << colors::kGreen << "2Q cache tests passed successfully\n" << colors::kReset;
+    std::cout << kLine;
 }
 
 static void RunIdealCacheTests() {
     std::cout << "Running ideal cache tests\n";
 
     for (const Test& test : kIdealCacheTests) {
+        std::cout << test.test_name_ << ": ";
+
         assert(test.requests_.size() == test.expected_hits_.size());
 
         caches::IdealCache<int> cache{test.cache_size_, test.requests_};
@@ -85,14 +98,51 @@ static void RunIdealCacheTests() {
             bool expected_hit = test.expected_hits_[i];
 
             if (actual_hit != expected_hit) {
-                throw std::runtime_error(colors::kRed + "Test " + test.test_name_ +
-                                         ": Request " + std::to_string(i + 1) + 
+                std::cout << colors::kRed << "failed" << colors::kReset << std::endl;
+
+                throw std::runtime_error(colors::kRed + "Request " + std::to_string(i + 1) + 
                                          " (key=" + std::to_string(test.requests_[i]) + 
                                          "): expected " + (expected_hit ? "HIT" : "MISS") + 
                                          ", got " + (actual_hit ? "HIT" : "MISS") + colors::kReset);
             }
         }
+
+        std::cout << colors::kGreen << "passed" << colors::kReset << std::endl;
     }
 
     std::cout << colors::kGreen << "Ideal cache tests passed successfully\n" << colors::kReset;
+    std::cout << kLine;
+}
+
+static void RunComparativeTests(size_t cache_capacity) {
+    std::cout << "Running comparative tests (cache_capacity = " << cache_capacity << ")\n";
+
+    size_t counter = 0;
+    auto ProcessTests = [&counter, cache_capacity](const std::vector<Test>& tests) {
+        for (const Test& test : tests) {
+            std::cout << "comparative_test_" << ++counter << " (based on " << test.test_name_ << "): ";
+
+            caches::TwoQCache<int> tq_cache{cache_capacity};
+            size_t hits_tq = 0;
+            for (int request : test.requests_) {
+                hits_tq += tq_cache.LookupUpdate(request, [](int x) { return x; });
+            }
+
+            caches::IdealCache<int> id_cache{cache_capacity, test.requests_};
+            size_t hits_id = 0;
+            for (int request : test.requests_) {
+                hits_id += id_cache.LookupUpdate(request, [](int x) { return x; });
+            }
+
+            std::cout << std::fixed << std::setprecision(1)
+                << colors::kBrightBlue << "TwoQCache: " << colors::kGreen << (100.0 * hits_tq) / test.requests_.size() << "% "
+                << colors::kBrightBlue << "IdealCache: " << colors::kGreen << (100.0 * hits_id) / test.requests_.size() << "%\n"
+                << colors::kReset;
+        }
+    };
+    
+    ProcessTests(kIdealCacheTests);
+    ProcessTests(kTwoQCacheTests);
+
+    std::cout << kLine;
 }
